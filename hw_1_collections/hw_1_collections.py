c8486a7689acf741c263c1455a224253031ee665
Aleksandr Voskoboinikov <al.a.voskoboinikov@yandex.ru>
2022-10-05 00:51:19 +0300

    Finalized all functionality. Kinda tested it. No additional functionality (or should I?..)

--- hw_1_collections/hw_1_collections.py
+++ hw_1_collections/hw_1_collections.py
@@ -1,3 +1,17 @@
+print('''Hi! I'm a python script that can work with a given RNA or DNA sequence. 
+For some reason I run in terminal and require manual input. Hope you will have at least some fun playing around with me)))0))0)
+
+Here's what I can do and understand:
+
+exit — завершение исполнения программы
+transcribe — напечатать транскрибированную последовательность
+reverse — напечатать перевёрнутую последовательность
+complement — напечатать комплементарную последовательность
+reverse complement — напечатать обратную комплементарную последовательность
+
+At least try to have some fun!
+''')
+
 nucl_comp_dct = {'RNA':{'A':'U', 'U':'A', 'G':'C', 'C':'G', 'a':'u', 'u':'a', 'g':'c', 'c':'g'}, 
                  'DNA':{'A':'T', 'T':'A', 'G':'C', 'C':'G', 'a':'t', 't':'a', 'g':'c', 'c':'g'}}
 
@@ -11,21 +25,32 @@ def is_seq_legit(seq):
     else:
         return True, 'DNA'
 
-def reverse(seq):
+def reverse(seq, nucl_type):
     print('Reversing...')
     return(seq[::-1])
 
-def transcribe(seq):
+def transcribe(seq, nucl_type):
     print('Transcribing...')
-    print(is_seq_legit(seq))
+    while nucl_type != 'DNA':
+        print('Your sequence is not DNA. Please, enter a new sequence')
+        seq = str(input('Enter seq: '))
+        nucl_type = is_seq_legit(seq)[1]
+    return seq.replace('T', 'U').replace('t', 'u')
 
-def complement(seq):
-    print('You look pretty today! UwU')
-    print(is_seq_legit(seq))
+def complement(seq, nucl_type):
+    print('Complementing...')
+    print('You look pretty today!')
+    seq = list(seq)
+    outseq = []
+    for letter in seq:
+        outseq.append(nucl_comp_dct[nucl_type][letter])
+    return(''.join(outseq))
 
-def reverse_complement(seq):
-    print('You look ugly today! -_-')
-    print(is_seq_legit(seq))
+
+def reverse_complement(seq, nucl_type):
+    print('Reverse complementing...')
+    print('You look pretty today!'[::-1])
+    return reverse(complement(seq, nucl_type), nucl_type)
 
 valid_command_dct = {'transcribe':transcribe, 'reverse':reverse, 
                      'complement':complement, 'reverse complement':reverse_complement}
@@ -36,13 +61,13 @@ while command != 'exit':
     if command in valid_command_dct:
         seq = str(input('Enter seq: '))
         while not is_seq_legit(seq)[0]:
-            print('Your sequence has invalid alphabet. Please, eneter a new sequence')
+            print('Your sequence has invalid alphabet. Please, enter a new sequence')
             seq = str(input('Enter seq: '))
-        print(valid_command_dct[command](seq))
+        nucl_type = is_seq_legit(seq)[1]
+        print(valid_command_dct[command](seq, nucl_type))
     else:
         print('Seems like an invalid command. Please, enter valid command!')
 
     command = str(input('Enter command: '))
 
 print('See you next time!')
-    
