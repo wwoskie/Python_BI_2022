5c505577fec41af5b585d23fda715467ddd05448
Aleksandr Voskoboinikov <al.a.voskoboinikov@yandex.ru>
2022-11-15 14:17:16 +0300

    finalized and most likely working version of the script

--- hw_4_numpy/numpy_challenge.py
+++ hw_4_numpy/numpy_challenge.py
@@ -12,21 +12,30 @@ if __name__ == "__main__":
 def matrix_multiplication(matrix1, matrix2):
     return np.matmul(matrix1, matrix2)
 
-def multiplication_check(matrix_lst): # so that multiplication will only work on 2d arrays
-    matrix_product = np.array(matrix_lst[0].shape)
+def multiplication_check(matrix_lst): 
+    prev_matrix = matrix_lst[0]
     can_multiply = True
-    for m in matrix_lst:
-        if m.ndim != 2:
+    for m in matrix_lst[1:]:
+        '''
+        multiplication check would fail for single matrices 
+        and if array is not a matrix (just an array)
+        also it would leave loop if can_multyply is already False
+        '''
+        if m.ndim != 2  or prev_matrix.ndim != 2 or len(matrix_lst) < 2 or can_multiply == False:
             can_multiply = False
             break
-        can_multiply = can_multiply and (matrix_product.shape[1] == m.shape[0])
+        can_multiply = can_multiply and (prev_matrix.shape[1] == m.shape[0])
+        prev_matrix = m
     return can_multiply
 
 def multiply_matrices(matrix_lst):
     if multiplication_check(matrix_lst):
-        matrix_product = np.array(matrix_lst[0].shape)
-        for m in matrix_lst:
-            matrix_product = matrix_multiplication(matrix_product, m)
+        matrix_product = matrix_multiplication(matrix_lst[0], matrix_lst[1])
+        if len(matrix_lst) > 2: # would go here only if matrix_lst is longer than 2
+            for m in matrix_lst[2:]:
+                matrix_product = matrix_multiplication(matrix_product, m)
+    else:
+        matrix_product = None
     return matrix_product
 
 def compute_2d_distance(m1, m2):
@@ -35,15 +44,5 @@ def compute_2d_distance(m1, m2):
 def compute_multidimensional_distance(m1, m2):
     return np.linalg.norm(m1 - m2)
 
-#def compute_pair_distances(array):
-
-
-matrix_lst = [np.array([0, 1, 3]), np.array([[1, 1], [1, 1]])]
-print(multiplication_check(matrix_lst))
-
-matrix_lst = [np.array([[4, 2], [9, 0]]), np.array([[3, 1], [-3, 4]])]
-print(multiplication_check(matrix_lst))
-
-print(multiply_matrices(matrix_lst))
-
-print(matrix_multiplication(matrix_lst[0], matrix_lst[1]))
+def compute_pair_distances(array):
+    return np.linalg.norm(array[:, None, :] - array[None, :, :], axis=-1)
